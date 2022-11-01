4c954b994147e9e92f892126c61ca376ec3853dd
petrovskinaum7 <116837683+petrovskinaum7@users.noreply.github.com>
2022-11-01 18:43:16 +0100

    Add files via upload

---
+++ Domasno1 INKI830.java
@@ -0,0 +1,11 @@
+package HelloWorld;
+
+public class HelloWorldOOPFikt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello World OOP FIKT!");
+	}
+
+}
